4637e3a784012dfb7eb12f7a558da389ab621cbf
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2023-02-11 00:24:02 +0530

    session 1 javaScript Basics

---
+++ feb/s1/s1.js
@@ -0,0 +1,45 @@
+// JavaScript is used for creating web pages
+//--------------------//
+//--------------------//
+// javaBasics
+// printing hello world
+console.log("Namaste Duniya");
+console.log(2);
+// Primitives Data Types
+console.log("numbers");//100,3
+console.log("Strings");//hello
+console.log("Booleans");//true
+console.log("Undefined");//undefined
+console.log("null");//null
+// Checking the data type
+let a = 10;
+let b = 'string';
+let c = true;
+// checking with typeof property
+console.log(typeof(a));//number
+console.log(typeof(b));//string
+console.log(typeof(c));//booleans
+// printing my name via variable
+let name = "Kuldeep";
+console.log(name);
+// -------------------------variables--------------------------
+// variables are named container used to store ,change ,and access information.
+// we first declare the variable and then we assign the value
+let var1;//variable declare 
+var1 = 5;//variable assign
+// ------------variable diffrence----------------
+// the diff is scope of variables and redeclaration and assigning values
+// -------------var-------------
+    // allowed redeclare
+    // allowed reassign
+// -------------let-------------
+    // allowed reassign 
+    // but not redeclare
+// -------------const-------------
+    // neither be reassign or redeclare
+// --------------------------------------------------------------------
+// ---------------namingVariable---------------------------------------
+// name start with letter
+// name start with $
+// name start with _underscore
+
